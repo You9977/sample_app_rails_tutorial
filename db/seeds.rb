1a66c4c7e1eafbc139d2cd097537cd67437b709e
You9977 <kaounakh0205@yahoo.co.jp>
2021-09-15 22:22:55 +0900

    fixed seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create!(name: "Example User", email: "examptorial.org",
+User.create!(name: "Example User", email: "example@railstutorial.org",
              password: "foobar", password_confirmation: "foobar", admin: true)
 
 99.times do |n|
