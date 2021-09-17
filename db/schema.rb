7fa457eac20e95a800d86f5d4827a89b360067ae
You9977 <kaounakh0205@yahoo.co.jp>
2021-09-17 23:18:33 +0900

    finish Chapter11.1-11.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_15_121259) do
+ActiveRecord::Schema.define(version: 2021_09_17_121813) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2021_09_15_121259) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.time "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
