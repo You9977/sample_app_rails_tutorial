66b028e32d8299c2726b2eec187e10f8f88ba73d
You9977 <kaounakh0205@yahoo.co.jp>
2021-09-18 12:33:27 +0900

    fixed Model:user

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,8 +42,8 @@ class User < ApplicationRecord
   
 
   def activate
-    update_attribute(:activated, true)
-    update_attribute(:activated_at, Time.zone.now)
+    self.update_attribute(:activated, true)
+    self.update_attribute(:activated_at, Time.zone.now)
   end
 
   def send_activation_email
